e89c86253ea88da101ab91b98965db604ea74a14
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-11-28 17:17:55 +0530

    add fx

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -1,7 +1,19 @@
 package lk.ijse;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/views/test.fxml")));
+        stage.setScene(scene);
+//        stage.setTitle("D-24");
+//        stage.getIcons().add(new Image("/assets/d23_logo.png"));
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
     }
 }
