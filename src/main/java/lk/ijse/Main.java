c839b51b8c04f6f6d06fc4d8046f74c19c6d7711
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-12-04 06:31:34 +0530

    add some non functional

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -3,6 +3,7 @@ package lk.ijse;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,8 +11,8 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/views/dashboardForm.fxml")));
         stage.setScene(scene);
-//        stage.setTitle("D-24");
-//        stage.getIcons().add(new Image("/assets/d23_logo.png"));
+        stage.setTitle("Car Rental System");
+        stage.getIcons().add(new Image("/img/logo/mini logo.png"));
         stage.centerOnScreen();
         stage.setResizable(false);
         stage.show();
